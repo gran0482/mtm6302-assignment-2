1e069c3446c2aa639f06ada3fbbf6914d0efd12d
gran0482 <gran0482@algonquinlive.com>
2021-10-22 13:23:55 -0400

    adding missing links and adding styles to css

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const $header = document.getElementById('header');
 const $container = document.getElementById('container');
 const $footer = document.getElementById('footer');
-const $images = []
+
+
 
 
 //Code
