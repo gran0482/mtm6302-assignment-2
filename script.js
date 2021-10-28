36f397b024aac32d4c2043219238cd0cc052c4f9
gran0482 <gran0482@algonquinlive.com>
2021-10-28 11:44:00 -0400

    setting up interactive features

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //Data
 const $header = document.getElementById('header');
 const $container = document.getElementById('container');
+const $button1 = document.getElementById('button');
+const $button2 = document.getElementById('button2');
+const $hover = document.getElementById('imageGallery');
 const $footer = document.getElementById('footer');
 // const $images = ['images/butterfly.jpg', 'images/geese.jpg','images/trail.jpg', 'images/greenCar.jpg', 'images/reflection.jpg', 'images/singleGoose.jpg', 'images/winterPath.jpg', 'images/crystalBall.jpg', 'images/watch.jpg', 'images/tree.jpg', 'images/leaf.jpg', 'images/lightTrail.jpg'];
 
@@ -18,21 +21,42 @@ const $images = [
     document.getElementById('imageGallery').src="images/leaf.jpg",
     document.getElementById('imageGallery').src="images/lightTrail.jpg",
 ];
-const $items = []
+
 
 //Code
-//reveal details about the image (shot types)
+//reveal details about the image on hover(shot types)
+$hover.addEventListener('mouseover', function(){
+  $hover.setAttribute(
+      $hover.innerHTML = 'image gallery',
+  )
+})
+
+$hover.addEventListener('mouseout', function (){
+  $hover.setAttribute(
+      'style', 'background-color: #F5F5F5'
+  )
+})
+
 
 //Showing a larger version of the image
+$button1.addEventListener('click', function(event){
+  console.log('button is working')
+})
+
+$button2.addEventListener('click', function(event){
+  console.log('button 2 is working')
+})
 
 //Run
 
 
 
-// for (let i=0; i < $images.length; i++) {
-//   const $images = document.getElementById('imageGallery')
-//   console.log($images[i]);    
-// }
+for (let i=0; i < $images.length; i++) {
+  // const $images = document.getElementById('imageGallery').innerHTML
+  const src = document.getElementById('imageGallery');
+
+  console.log($images[i]);    
+}
 
 
 //header and footer text
