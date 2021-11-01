bde5315b3c9b9d118efb993d7deba4f54520c621
gran0482 <gran0482@algonquinlive.com>
2021-11-01 11:21:42 -0400

    removing code that is no longer needed

--- script.js
+++ script.js
@@ -5,22 +5,14 @@ const $button1 = document.getElementById('button');
 const $button2 = document.getElementById('button2');
 const $hover = document.getElementById('imageGallery');
 const $footer = document.getElementById('footer');
-// const $images = ['images/butterfly.jpg', 'images/geese.jpg','images/trail.jpg', 'images/greenCar.jpg', 'images/reflection.jpg', 'images/singleGoose.jpg', 'images/winterPath.jpg', 'images/crystalBall.jpg', 'images/watch.jpg', 'images/tree.jpg', 'images/leaf.jpg', 'images/lightTrail.jpg'];
+const $images = ['images/butterfly.jpg', 'images/geese.jpg','images/trail.jpg', 'images/greenCar.jpg', 'images/reflection.jpg', 'images/singleGoose.jpg', 'images/winterPath.jpg', 'images/crystalBall.jpg', 'images/watch.jpg', 'images/tree.jpg', 'images/leaf.jpg', 'images/lightTrail.jpg'];
 
-const $images = [ 
-    document.getElementById('imageGallery').src="images/butterfly.jpg",
-    document.getElementById('imageGallery').src="images/geese.jpg",
-    document.getElementById('imageGallery').src="images/trail.jpg",
-    document.getElementById('imageGallery').src="images/greenCar.jpg",
-    document.getElementById('imageGallery').src="images/reflection.jpg",
-    document.getElementById('imageGallery').src="images/singleGoose.jpg",
-    document.getElementById('imageGallery').src="images/winterPath.jpg",
-    document.getElementById('imageGallery').src="images/crystalBall.jpg",
-    document.getElementById('imageGallery').src="images/watch.jpg",
-    document.getElementById('imageGallery').src="images/tree.jpg",
-    document.getElementById('imageGallery').src="images/leaf.jpg",
-    document.getElementById('imageGallery').src="images/lightTrail.jpg",
-];
+const animals = ['dog', 'cat', 'mouse']
+
+for (const animal of animals) {
+  console.log(animals)
+}
+
 
 
 //Code
@@ -53,12 +45,13 @@ $button2.addEventListener('click', function(event){
 
 
 
-for (let i=0; i < $images.length; i++) {
-  // const $images = document.getElementById('imageGallery').innerHTML
-  const src = document.getElementById('imageGallery');
+// for (let i=0; i < $images.length; i++) {
+//   // const $images = document.getElementById('imageGallery').innerHTML
+//   const src = document.getElementById('imageGallery');
 
-  console.log($images[i]);    
-}
+//   console.log($images[i]);    
+// }
+
 
 
 //header and footer text
