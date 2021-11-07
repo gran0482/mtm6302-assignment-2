a9de1d1a051ce5f46d5f8e559d5c025e724dda5f
gran0482 <gran0482@algonquinlive.com>
2021-11-07 09:34:29 -0500

    making the for loop function properly

--- script.js
+++ script.js
@@ -6,24 +6,22 @@ const $button2 = document.getElementById('button2');
 const $footer = document.getElementById('footer');
 const $hover = document.getElementById('imageGallery');
 const $images = ['images/butterfly.jpg', 'images/geese.jpg','images/trail.jpg', 'images/greenCar.jpg', 'images/reflection.jpg', 'images/singleGoose.jpg', 'images/winterPath.jpg', 'images/crystalBall.jpg', 'images/watch.jpg', 'images/tree.jpg', 'images/leaf.jpg', 'images/lightTrail.jpg'];
-
-
-for (const image of $images){
-
-  const img = document.querySelector('img');
-  img.src = 'images/butterfly.jpg'
-
-
-}
-
-console.log($images)
-
-
+const $shotTypes = ['Macro', 'Wildlife', 'leading lines', 'automotive', 'Nature reflection', 'wildlife', 'seasonal', 'mirror reflection', 'product', 'Nature reflection', 'Nature', 'long exposure'];
 
 //Code
+
+let text = '';
+for (const image of $images){
+
+  text += '<img src="' + image + '" alt=""></img>';
+  
+  console.log(image)
+}
+document.getElementById('container').innerHTML = text;
+
 //reveal details about the image on hover(shot types)
 
-// Macro, Wildlife, leading lines, automotive, Nature reflection, wildlife (2), seasonal, mirror reflection, product, Nature reflection(2), Nature, long exposure
+// Macro, Wildlife, leading lines, automotive, Nature reflection, wildlife, seasonal, mirror reflection, product, Nature reflection, Nature, long exposure
 
 $hover.addEventListener('mouseover', function(){
   $hover.setAttribute('style', `
@@ -41,18 +39,22 @@ $hover.addEventListener('mouseout', function (){
   )
 })
 
+// https://stackoverflow.com/questions/3559467/description-box-using-onmouseover
+
 
 //Showing a larger version of the image
+img = document.getElementById('imageGallery')
+
 $button1.addEventListener('click', function(event){
-  // img.style.transform = "scale(2)"
-  // img.style.transition = "transform 0.5s ease-in-out";
+  img.style.transform = "scale(1.5)"
+  img.style.transition = "transform 0.5s ease-in-out";
 
   console.log('button is working')
 })
 
 $button2.addEventListener('click', function(event){
-  // img.style.transform = "scale(1)"
-  // img.style.transition = "transform 0.5s ease-in-out";
+  img.style.transform = "scale(1)"
+  img.style.transition = "transform 0.5s ease-in-out";
 
   console.log('button 2 is working')
 })
